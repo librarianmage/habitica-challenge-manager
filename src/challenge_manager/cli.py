23965970a9c53eebc5f71295398adacdbb0b6999
MathWhiz <raymond.arndorfer@gmail.com>
2017-10-05 14:31:55 -0400

    Add api credential validation

--- src/challenge_manager/cli.py
+++ src/challenge_manager/cli.py
@@ -14,18 +14,35 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
-import click
+import click, re
 
+class HabiticaUUIDParamType(click.ParamType):
+  name = "UUID"
+  
+  def convert(self, value, param, ctx):
+    regex = re.compile('[a-zA-Z0-9]{8}-(?:[a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{12}')
+    try:
+      assert regex.fullmatch(value) is not None
+      return value
+    except AssertionError:
+      self.fail('%s is not a valid UUID' % value, param, ctx)
+
+HabiticaUUID = HabiticaUUIDParamType()
 
 @click.group()
-@click.option('-u', '--id', 'userId', prompt=True)
-@click.option('-k', '--key', 'apiKey', prompt=True, hide_input=True)
+@click.option('-u', '--id', 'userID', type=HabiticaUUID, prompt='User ID')
+@click.option('-k', '--key', 'apiKey', type=HabiticaUUID, prompt='API Key')
 @click.pass_context
-def cli(ctx):
-  ctx.obj['']
+def cli(ctx, userID, apiKey):
+  ctx.obj = {
+    'AUTH': {
+      'x-api-user': userID,
+      'x-api-key': apiKey
+    }
+  }
 
 @cli.command()
 @click.argument('files', nargs=-1, type=click.Path(exists=True))
 @click.pass_context
 def upload(ctx, files):
-  click.echo('upload')
+  click.echo('uploading to user %s' % ctx.obj['AUTH']['x-api-user'])
